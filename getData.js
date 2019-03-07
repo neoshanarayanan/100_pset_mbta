94241b73f516d54639017d05fc6161c8969c7f91
neoshanarayanan <neosha@mit.edu>
2019-03-06 23:37:52 -0500

    finalized hw

--- getData.js
+++ getData.js
@@ -1,17 +1,21 @@
 
 // get data for route 1 using API key
 function makeCall(){
+    var vehicles = [];
     fetch("https://api-v3.mbta.com/vehicles?api_key=b71590178cb04823bffb3c3b1bf0725b&filter[route]=1&include=trip")
     .then(response => response.json())
     .then(json => {
-        var vehicles = json.data;
+        vehicles.push(json.data);
     })
+
+console.log(vehicles); 
 }
 
 var counter = 0;
+var numCycles = 100;
 
 function run(){
-    if(counter < 100){
+    if(counter < numCycles){
         makeCall();
         setTimeout(run, 15000);
     }
